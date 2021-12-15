35d0d5a6cac20707180efd219273d2cf71c99a51
Wong Cheng Ngai <wcngai@gmail.com>
2021-12-15 17:36:08 +0800

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -3,6 +3,9 @@ const sonarqubeScanner = require('sonarqube-scanner');
        serverUrl: 'http://192.168.40.103:9000',
        options : {
        'sonar.sources': '.',
+       'sonar.login':'admin',
+       'sonar.password':'Admin,1',
+  
        //'sonar.inclusions' : '.' // Entry point of your code
        }
      }, () => {});
